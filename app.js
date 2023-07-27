3a0ee8a4c4750c4dddb4f63d4c4214730732bb83
bhanukosuri <61044101+bhanukosuri@users.noreply.github.com>
2023-07-27 12:49:05 +0530

    Chat Application after including controllers

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
+const path = require('path');
 
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const loginRouter = require('./routes/login')
-const chatRouter = require('./routes/postChat')
-const contactUsRouter = require('./routes/contactUs')
+const errorController = require('./controllers/error')
 
-const bodyParser = require('body-parser')
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, "public")))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(loginRouter)
-app.use(chatRouter)
-app.use(contactUsRouter)
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000)
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
